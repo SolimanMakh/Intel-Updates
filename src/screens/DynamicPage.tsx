6974db518cde8d33bf7b6e30b07087e32b7e122d
SolimanTJB <solimanmakh@gmail.com>
2025-11-05 22:14:23 +0200

    remove unused vars & imports

--- src/screens/DynamicPage.tsx
+++ src/screens/DynamicPage.tsx
@@ -1,4 +1,4 @@
-import { useLoaderData, useSearchParams, Link } from "react-router-dom";
+import { useLoaderData, useSearchParams } from "react-router-dom";
 import AppShell from "../ui/layout/AppShell";
 import Grid from "../ui/layout/Grid";
 import FiltersSidebar from "../ui/filters/FiltersSidebar";
@@ -24,7 +24,7 @@ export async function dynamicLoader({ params, request }: LoaderFunctionArgs) {
 
 export default function DynamicPage() {
   const { schema, filtered, sectors, countries } = useLoaderData() as any;
-  const [sp] = useSearchParams();
+  // const [sp] = useSearchParams();
 
   return (
     <AppShell title={schema.title}>
